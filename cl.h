f3b3ed4f1f87ab6dad2e1e357219830e0247f6a9
FaDDeZ <139443537+FaDDeZ@users.noreply.github.com>
2023-07-29 18:37:08 +0300

    Create cl.h

---
+++ cl.h
@@ -0,0 +1,24 @@
+#ifndef cl_h
+#define cl_h
+#include "base.h"
+class Cl_2 : public Base {
+public:
+	Cl_2(Base* parent, string name, int status);
+};
+class Cl_3 : public Base {
+public:
+	Cl_3(Base* parent, string name, int status);
+};
+class Cl_4 :public Base {
+public:
+	Cl_4(Base* parent, string name, int status);
+};
+class Cl_5 : public Base {
+public:
+	Cl_5(Base* parent, string name, int status);
+};
+class Cl_6 :public Base {
+public:
+	Cl_6(Base* parent, string name, int status);
+};
+#endif
