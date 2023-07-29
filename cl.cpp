7a6cc78eb23982994fe261091aa60505fea44f37
FaDDeZ <139443537+FaDDeZ@users.noreply.github.com>
2023-07-29 18:38:07 +0300

    Create cl.cpp

---
+++ cl.cpp
@@ -0,0 +1,6 @@
+#include "cl.h"
+Cl_2::Cl_2(Base* parent, string name, int status) :Base(parent, name, status) {};
+Cl_3::Cl_3(Base* parent, string name, int status) :Base(parent, name, status) {};
+Cl_4::Cl_4(Base* parent, string name, int status) :Base(parent, name, status) {};
+Cl_5::Cl_5(Base* parent, string name, int status) :Base(parent, name, status) {};
+Cl_6::Cl_6(Base* parent, string name, int status) :Base(parent, name, status) {};
